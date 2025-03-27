b62a6b2c591b729f904630d17a6664411d293eb7
diegogr94 <diegoguerreromil994@gmail.com>
2025-03-27 12:45:56 +0100

    cambioDosDiego

--- src/LecturaDeFichero.java
+++ src/LecturaDeFichero.java
@@ -1,8 +1,9 @@
 import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class LecturaDeFichero {
-    
+
     public String leer(String carpetaSeleccionada, Scanner sc) {
         System.out.print("Dime el nombre del archivo: ");
         String archivoSeleccionado = sc.nextLine();
@@ -13,5 +14,19 @@ public class LecturaDeFichero {
             System.out.println("Lo siento, este archivo no existe ( o no es valido ).");
             return null;
         }
+    }
+
+    public String leerDatos(String carpetaSeleccionada, String archivoSeleccionado) {
+        File archivo = new File(carpetaSeleccionada, archivoSeleccionado);
+        StringBuilder datos = new StringBuilder();
+        try (Scanner fs = new Scanner(archivo)) {
+            while (fs.hasNextLine()) {
+                datos.append(fs.nextLine()).append("\n");
+            }
+        } catch (IOException e) {
+            System.out.println("Ha habido un error al leer el archivo: " + e.getMessage());
+        }
+
+        return datos.toString().trim();
     }
 }
