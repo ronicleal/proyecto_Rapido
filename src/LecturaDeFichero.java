972da5bed97ee32ca24e3bc9f52ebd437af5aca9
diegogr94 <diegoguerreromil994@gmail.com>
2025-03-27 18:41:19 +0100

    lecturaFicheroMejorada

--- src/LecturaDeFichero.java
+++ src/LecturaDeFichero.java
@@ -1,6 +1,9 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 
 public class LecturaDeFichero {
 
@@ -8,25 +11,27 @@ public class LecturaDeFichero {
         System.out.print("Dime el nombre del archivo: ");
         String archivoSeleccionado = sc.nextLine();
         File archivo = new File(carpetaSeleccionada, archivoSeleccionado);
-        if (archivo.exists()) {
-            return archivoSeleccionado;
-        } else {
+
+        if (archivo.exists() && archivo.isFile()) {
+            StringBuilder contenido = new StringBuilder();
+
+            try(BufferedReader br = new BufferedReader(new FileReader(archivo))){
+                String linea;
+                while((linea = br.readLine()) != null){
+                    contenido.append(linea).append("\n");
+                }
+
+            System.out.println("Contenido del archivo:\n" + contenido.toString());
+            
+            }catch (IOException e){
+                System.out.println("Ha habido un error al leer el archivo: " + e.getMessage());
+                return null;
+            }
+            return contenido.toString();
+        }else{
             System.out.println("Lo siento, este archivo no existe ( o no es valido ).");
             return null;
         }
-    }
-
-    public String leerDatos(String carpetaSeleccionada, String archivoSeleccionado) {
-        File archivo = new File(carpetaSeleccionada, archivoSeleccionado);
-        StringBuilder datos = new StringBuilder();
-        try (Scanner fs = new Scanner(archivo)) {
-            while (fs.hasNextLine()) {
-                datos.append(fs.nextLine()).append("\n");
-            }
-        } catch (IOException e) {
-            System.out.println("Ha habido un error al leer el archivo: " + e.getMessage());
-        }
-
-        return datos.toString().trim();
+       
     }
 }
