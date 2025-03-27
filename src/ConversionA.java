907f47630d7ba38acd8eca07e5c0c686ee34fbc3
diegogr94 <diegoguerreromil994@gmail.com>
2025-03-28 00:44:49 +0100

    primeraConversion

---
+++ src/ConversionA.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ConversionA {
+
+    public void convertir(Scanner sc, String carpetaSeleccionada) {
+        
+
+        System.out.println("Selecciona el formato de salida:");
+        System.out.println("1. CSV");
+        System.out.println("2. JSON");
+        System.out.println("3. XML");
+        System.out.print("Pon el número del formato de salida: ");
+        int formato = sc.nextInt();
+       
+
+        System.out.print("Escribe el nombre del archivo de salida (sin extensión): ");
+        String nombreSalida = sc.nextLine();
+
+     
+}
+}
