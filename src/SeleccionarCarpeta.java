865a5a37d58f0b628b3fa0c487e1c48b058d92d4
ronic <ralealp01@iesalbarregas.es>
2025-03-27 18:44:42 +0100

    arreglo_seleccionarCarpeta

--- src/SeleccionarCarpeta.java
+++ src/SeleccionarCarpeta.java
@@ -1,23 +1,23 @@
 import java.io.File;
 import java.util.Scanner;
+
 public class SeleccionarCarpeta {
     public String seleccionar(Scanner sc) {
         System.out.println("Ingrese la ruta de la carpeta: ");
         String carpetaSelecionada = sc.nextLine();
         File carpeta = new File(carpetaSelecionada);
-        if(carpeta.exists() && carpeta.isDirectory()){
+        if (carpeta.exists() && carpeta.isDirectory()) {
             System.out.println("Carpeta seleccionada: " + carpetaSelecionada);
             System.out.println("Contenido de la carpeta:");
-            for(String archivo : carpeta.list()){
+            for (String archivo : carpeta.list()) {
                 System.out.println("- " + archivo);
             }
             return carpetaSelecionada;
-        }else{
+        } else {
             System.out.println("La carpeta no existe o no es válida.");
             return "";
         }
 
-
     }
-    
+
 }
